1afe3065409b2cd5748dfad21b977b308ef2b215
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-09-07 20:16:55 +0000

    indentation issue

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,9 +9,11 @@ else:
 if last_number > 5:
     print('Last digit of {:d} is {:d} and is greater than 5'
           .format(number, last_number))
+
 elif last_number == 0:
     print('Last digit of {:d} is {:d} and is 0'
-	  .format(number, last_number))
+          .format(number, last_number))
+
 else:
     print('Last digit of {:d} is {:d} and is less than 6 and not 0'
 	  .format(number, last_number))
