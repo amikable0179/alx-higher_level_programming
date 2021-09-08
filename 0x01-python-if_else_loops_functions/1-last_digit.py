5dc8c5a7b3b75519466e03e031149b960153b006
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-09-08 10:56:04 +0000

    pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,4 +13,4 @@ elif last_number == 0:
           .format(number, last_number))
 else:
     print('Last digit of {:d} is {:d} and is less than 6 and not 0'
-	  .format(number, last_number))
+          .format(number, last_number))
