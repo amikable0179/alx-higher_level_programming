1a9e228734ce6849a9068167de7f6577d0e96fb8
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-11-28 13:55:17 +0000

    removed repeated words

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,4 @@
 def multiple_returns(sentence):
     lenght = len(sentence)
     char = sentence[0] if length > 0 else None
-    return (length, char)#!/usr/bin/python3
-def multiple_returns(sentence):
-    lenght = len(sentence)
-    char = sentence[0] if length > 0 else None
     return (length, char)
