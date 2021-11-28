80ad310217b265eed33ed60e73da979929490c1b
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-11-28 13:42:05 +0000

    6-10

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    lenght = len(sentence)
+    char = sentence[0] if length > 0 else None
+    return (length, char)#!/usr/bin/python3
+def multiple_returns(sentence):
+    lenght = len(sentence)
+    char = sentence[0] if length > 0 else None
+    return (length, char)
