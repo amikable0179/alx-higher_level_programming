531605e788b7413f434ff0fe47c948824ae70851
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-12-08 06:34:04 +0000

    whitespace

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,9 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """ creates a private instance attribute size  """
 
 
 class Square:
-    """ this is the empty class """
+    """ this is a class with a private attribute size """
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("type must be an integer")
